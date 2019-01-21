87d92a5fac5aba66cbc9832b9773da6d22798f93
Andreas <andreasnorstein@gmail.dom>
2019-01-21 14:39:37 +0100

    initial project setup

--- README.md
+++
@@ -1,1 +0,0 @@
-# legendary-octo-sniffle
---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        BorderPane borderPane = new BorderPane();
+        Scene scene = new Scene(borderPane, 1200, 800);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
